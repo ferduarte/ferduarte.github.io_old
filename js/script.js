f2bbb37d0745f9430d4317a64e0b3391b73ec2f7
Sidney Leal <sidney.leal@hpe.com>
2017-03-02 19:45:32 -0300

    https

--- js/script.js
+++ js/script.js
@@ -179,7 +179,7 @@ $(document).ready(function () {
 (function ($) {
     var o = document.getElementById("google-map");
     if (o) {
-        include('http://maps.google.com/maps/api/js?sensor=false');
+        include('https://maps.google.com/maps/api/js?sensor=false');
         include('js/jquery.rd-google-map.js');
 
         $(document).ready(function () {
